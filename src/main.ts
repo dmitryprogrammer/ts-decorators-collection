d15fd547955376a7cd4946eb734d464aeb041a91
dmitryprogrammer <dpischalka@gmail.com>
2025-04-07 00:04:41 +0300

    feat(log): added log exe.

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,15 @@ class Test {
   testLogMethod() {
     console.log("testLogMethod01");
   }
+
+  @log("testAfterMessage", { position: "after" })
+  testLogAfterMethod() {
+    console.log("testLogMethod02");
+    
+  }
 }
 
 const test = new Test();
 test.testLogMethod();
+console.log("-------------------------");
+test.testLogAfterMethod();
